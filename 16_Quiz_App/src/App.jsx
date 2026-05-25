30d309f3febcf391ebc727e673634e509726e653
FarhanAhmad-byte <farhan989800@gmail.com>
2026-05-25 16:19:29 +0530

    Add all functionality need to add result page

--- 16_Quiz_App/src/App.jsx
+++ 16_Quiz_App/src/App.jsx
@@ -5,9 +5,9 @@ import useData from "./hooks/useData"
 function StartUP({setFunc}) {
   return (
     <div
-      className="flex flex-col items-center text-2xl font-sans border p-3 rounded-3xl"
+      className="w-full border border-zinc-700 flex flex-col items-center text-2xl font-sans p-3 rounded-3xl gap-5"
     >
-      <h1 className="text-center">Info</h1>
+      <h1 className="text-center text-4xl py-4 px-10 rounded-[5px] bg-orange-400">Quiz</h1>
       <ul className="list-disc p-6">
         <li>This is a Multiple Choice based quiz</li>
         <li>There is not any negative markings</li>
@@ -15,7 +15,7 @@ function StartUP({setFunc}) {
       </ul>
       <button
         onClick={() => { setFunc((prev) => !prev) }}
-        className="text-3xl font-bold border py-5 px-10 rounded-3xl text-white bg-red-700 hover:scale-105 transition-all hover:bg-red-800"
+        className="text-3xl font-bold py-5 px-10 rounded-[5px] text-white bg-gray-700 hover:scale-105 transition-all hover:bg-gray-500"
       >
         Start
       </button>
@@ -27,12 +27,12 @@ function QuizLayout() {
   const data = useData()
 
   if(!data.length){
-    return <p className="bg-red-700 text-white text-3xl">Loading...</p>
+    return <p className="bg-gray-700 text-white text-3xl">Loading...</p>
   }
 
   return (
       <div
-        className="flex flex-col border border-black rounded-3xl p-3 gap-5"
+        className="flex flex-col items-center w-full p-3 gap-5"
       >
         <Quiz data={data}/>
       </div>
@@ -43,7 +43,7 @@ function App() {
   const [start, setStart] = useState(true)
 
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col gap-5">
+    <div className="bg-gray-800 w-full min-h-screen flex justify-center items-center flex-col gap-5 text-white">
         {(start) ? ( <StartUP setFunc={setStart} /> ) : ( <QuizLayout /> )}
     </div>
   );
--- 16_Quiz_App/src/components/Quiz.jsx
+++ 16_Quiz_App/src/components/Quiz.jsx
@@ -1,24 +1,84 @@
 /* eslint-disable no-useless-assignment */
 import { useState } from "react";
 
-function QuizData({currentQuestion, changeQuestion, currentIndex, dataLastIndex, setSubmit}) {
+function QuizData({currentQuestion, changeQuestion, currentIndex, dataLastIndex, setSubmitState, score, setScore}) {
+
+  function inputChange(element, question, score) {
+    const inputs = document.querySelectorAll("input")    
+    const parent = element.target.parentElement
+
+    if (question.Answer === element.target.value) {
+      parent.style.background = "green"
+      parent.style.color = "white"
+      score((prev) => prev += 1)
+    }
+    else{
+      parent.style.background = "red"
+      parent.style.color = "white"
+    }
+    // disable every other inputs 
+    Array.from(inputs).map((input) => {
+      input.disabled = true
+    })
+    setShowButton(true)
+  }
+
+  const [showButton, setShowButton] = useState(false)
+
   return (
-    <div>
-      <h1>Question {currentQuestion?.no}</h1>
-      <p>{currentQuestion?.Question}</p>
-      <form>
-        <input type="radio" id="option1" value={currentQuestion?.Options[0]} />
-        <label for="option1">{currentQuestion?.Options[0]}</label>
-        <input type="radio" id="option2" value={currentQuestion?.Options[1]} />
-        <label for="option2">{currentQuestion?.Options[1]}</label>
-        <input type="radio" id="option3" value={currentQuestion?.Options[2]} />
-        <label for="option3">{currentQuestion?.Options[2]}</label>
-        <input type="radio" id="option4" value={currentQuestion?.Options[3]} />
-        <label for="option4">{currentQuestion?.Options[3]}</label>
+    <div className="w-120 flex flex-col items-center border border-zinc-700 p-5 rounded-2xl">
+      <div className="border-b-2 border-white w-full mx-2 text-2xl p-2 flex justify-between">
+          <h1>Quiz {currentQuestion?.no}</h1>
+          <h2 className="text-xl">Score: {score}</h2>
+      </div>
+      <p className="py-5 text-3xl">{currentQuestion?.Question}</p>
+      <form className="flex flex-col gap-3"
+        >
+        <div className="border border-white bg-white text-black text-2xl flex gap-5 rounded-4xl w-100">
+          <input onClick={(e) => { inputChange(e, currentQuestion, setScore) }}
+          className="size-5 self-center color m-2"
+          name="choice"
+          type="radio"
+          id="option1"
+          value={currentQuestion?.Options[0]} />
+          <label htmlFor="option1">{currentQuestion?.Options[0]}</label>
+        </div>
+
+        <div className="border border-white bg-white text-black text-2xl flex gap-5 rounded-4xl w-100">
+          <input onClick={(e) => { inputChange(e, currentQuestion, setScore) }}
+          className="size-5 self-center color m-2"
+          name="choice"
+          type="radio"
+          id="option2"
+          value={currentQuestion?.Options[1]} />
+          <label htmlFor="option2">{currentQuestion?.Options[1]}</label>
+        </div>
+
+        <div className="border border-white bg-white text-black text-2xl flex gap-5 rounded-4xl w-100">
+          <input onClick={(e) => { inputChange(e, currentQuestion, setScore) }}
+          className="size-5 self-center color m-2"
+          name="choice"
+          type="radio"
+          id="option3" 
+          value={currentQuestion?.Options[2]} />
+          <label htmlFor="option3">{currentQuestion?.Options[2]}</label>
+        </div>
+        
+        <div className="border border-white bg-white text-black text-2xl flex gap-5 rounded-4xl w-100">
+          <input onClick={(e) => { inputChange(e, currentQuestion, setScore) }}
+          className="size-5 self-center color m-2"
+          name="choice"
+          type="radio"
+          id="option4"
+          value={currentQuestion?.Options[3]} />
+          <label htmlFor="option4">{currentQuestion?.Options[3]}</label>
+        </div>
       </form>
-      <button onClick={changeQuestion}>Next Question</button>
-      {currentIndex === dataLastIndex ? (
-        <button onClick={ setSubmit(false) } className="bg-red p-4 border rounded">
+
+      {currentIndex < dataLastIndex && showButton ? (<button className="text-2xl font-bold py-5 px-10 rounded-[5px] text-white bg-gray-700 hover:scale-105 transition-all hover:bg-gray-500 mt-5" onClick={ () => changeQuestion( setShowButton ) }>Next Question</button>) : null}
+
+      {currentIndex === dataLastIndex && showButton ? (
+        <button onClick={ () => setSubmitState(true) } className="text-2xl font-bold py-5 px-10 rounded-[5px] text-white bg-gray-700 hover:scale-105 transition-all hover:bg-gray-500 mt-5">
           Submit
         </button>
       ) : null }
@@ -26,20 +86,29 @@ function QuizData({currentQuestion, changeQuestion, currentIndex, dataLastIndex,
   );
 }
 
-function Result() {
+function Result({result, dataLastIndex}) {
+
   return(
-    <h1>You passed</h1>
+    <div className="bg-orange-300">
+      <h1 className="border-b border-white">Result</h1>
+      <h1>Total Questions {dataLastIndex + 1}</h1>
+      <p>Correct Answer {result}</p>
+      <p>{(dataLastIndex + 1)/2 < result ? "passed" : "failed"}</p>
+    </div>
   )
 }
 
 export default function Quiz({ data }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [submit, setSubmit] = useState(true);
+  const [submitted, setSubmitted] = useState(false);
+  const [score, setScore] = useState(0)
 
-  const changeQuestion = () => {
+  const incrementQuestionIndex = (setShowButtonState) => {
+    // increment current state if it is less than last index of the data and set show button state to false
     setCurrentIndex((prev) =>
       prev < dataLastIndex ? (prev += 1) : (prev = dataLastIndex),
     );
+    setShowButtonState(false)
   };
 
   const dataLastIndex = data.length - 1;
@@ -48,9 +117,9 @@ export default function Quiz({ data }) {
   return (
     <div
       key={currentQuestion?.no}
-      className="flex flex-col items-center justify-center border border-black"
+      className=""
     >
-      {submit ? <QuizData changeQuestion={changeQuestion} currentQuestion={currentQuestion} currentIndex={currentIndex} dataLastIndex={dataLastIndex} setSubmit={setSubmit} /> : <Result />}
+      {submitted ? <Result result={score} dataLastIndex={dataLastIndex} /> : <QuizData changeQuestion={ incrementQuestionIndex } currentQuestion={currentQuestion} currentIndex={currentIndex} dataLastIndex={dataLastIndex} setSubmitState={setSubmitted} score={score} setScore={setScore} />}
     </div>
   );
 }
